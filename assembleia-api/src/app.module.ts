aaea0a74656614b18c1319950d62aa96a0628e28
GeorgeSalu <george.salu10@gmail.com>
2024-04-07 17:57:13 -0300

    gerando o modulo people

--- assembleia-api/src/app.module.ts
+++ assembleia-api/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HelloControler } from './hello.controller';
 import { HelloService } from './hello.service';
+import { PeopleModule } from './people/people.module';
 
 @Module({
-    imports: [],
+    imports: [PeopleModule],
     controllers: [AppController, HelloControler],
     providers: [AppService, HelloService],
 })
