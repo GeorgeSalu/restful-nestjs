f1fa32756e80a1800ba64ac0c26d66c51ec06399
GeorgeSalu <george.salu10@gmail.com>
2024-04-07 17:06:09 -0300

    corrige identação

--- assembleia-api/src/hello.controller.ts
+++ assembleia-api/src/hello.controller.ts
@@ -1,16 +1,13 @@
-import { Controller, Get } from "@nestjs/common";
-import { HelloService } from "./hello.service";
+import { Controller, Get } from '@nestjs/common';
+import { HelloService } from './hello.service';
 
 @Controller()
 export class HelloControler {
 
-  constructor(
-    private helloService: HelloService
-  ){}
+    constructor(private helloService: HelloService) {}
 
-  @Get("/hello")
-  hello(): string {
-    return this.helloService.hello(" fulano");
-  }
-
+    @Get('/hello')
+    hello(): string {
+        return this.helloService.hello(' fulano');
+    }
 }
--- assembleia-api/src/hello.service.ts
+++ assembleia-api/src/hello.service.ts
@@ -1,10 +1,9 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class HelloService {
 
-  hello(name: string): string {
-    return `hello ${name}`;
-  }  
-
+    hello(name: string): string {
+        return `hello ${name}`;
+    }
 }
