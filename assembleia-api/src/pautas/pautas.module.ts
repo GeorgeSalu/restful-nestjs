ba63984847f1e9ebaf09adc331aa68a601ba41c0
GeorgeSalu <george.salu10@gmail.com>
2024-04-10 18:14:32 -0300

    criando os service de pautas

--- assembleia-api/src/pautas/pautas.module.ts
+++ assembleia-api/src/pautas/pautas.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from 'src/database/database.module';
+import { PautasService } from './pautas.service';
 
 @Module({
-    imports: [DatabaseModule]
+    imports: [DatabaseModule],
+    providers: [PautasService]
 })
 export class PautasModule {}
