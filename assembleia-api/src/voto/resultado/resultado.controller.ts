348da9e3c60599cf74248f025bd8cfd8bcb11cfe
GeorgeSalu <george.salu10@gmail.com>
2024-04-13 10:57:45 -0300

    implementando do contrato de endppoint de resultado de votacao

--- assembleia-api/src/voto/resultado/resultado.controller.ts
+++ assembleia-api/src/voto/resultado/resultado.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Param, Res } from '@nestjs/common';
+import { VotoService } from '../voto.service';
+import { Response } from 'express';
 
-@Controller('resultado')
-export class ResultadoController {}
+@Controller('pautas/:id/resultados')
+export class ResultadoController {
+
+    constructor(
+        private readonly votoService: VotoService
+    ){}
+
+    @Get()
+    async obterResultado(@Param('id') idPauta: string,@Res() response: Response) {
+        return response.status(HttpStatus.OK).send();
+    }
+
+}
