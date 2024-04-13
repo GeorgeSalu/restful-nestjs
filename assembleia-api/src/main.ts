a6f3bf9ac3712936aec078bc50f53bb8b02fb9f1
GeorgeSalu <george.salu10@gmail.com>
2024-04-13 12:29:16 -0300

    configuracao do swagger

--- assembleia-api/src/main.ts
+++ assembleia-api/src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  
+  const swaggerConfig = new DocumentBuilder()
+                              .setTitle("assembleia api")
+                              .setDescription("api do projeto assembleia")
+                              .setVersion('1.0.0')
+                              .setContact('george', 'http:/api.com.br', 'api')
+                              .build();
+
+  const swaggerDoc = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('api', app, swaggerDoc);
+
   await app.listen(3000);
 }
 bootstrap();
