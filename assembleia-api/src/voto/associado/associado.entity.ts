6aa1b14fa592f8e7e042791a17e39342d8584674
GeorgeSalu <george.salu10@gmail.com>
2024-04-12 21:19:13 -0300

    mapeamento da entidade de associado

--- assembleia-api/src/voto/associado/associado.entity.ts
+++ assembleia-api/src/voto/associado/associado.entity.ts
@@ -1,3 +1,12 @@
+import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
 export class Associado {
     
+    @PrimaryGeneratedColumn()
+    id?: string;
+
+    @Column()
+    cpf: string;
+
 }
