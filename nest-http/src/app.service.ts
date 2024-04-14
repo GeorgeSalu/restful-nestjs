1bbd7b97975d1af4dcd9e5d023f0a2fb8a3c97b7
GeorgeSalu <george.salu10@gmail.com>
2024-04-14 16:00:55 -0300

    nest-http - chamada http usando get

--- nest-http/src/app.service.ts
+++ nest-http/src/app.service.ts
@@ -1,8 +1,25 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  
+  constructor(
+    private readonly httpService: HttpService
+  ){}
+
+  async getAllTodos() {
+    let todos = [];
+    
+    const url = "http://jsonplaceholder.typicode.com/todos";
+
+    const { status, data } = await this.httpService.get<ITodo[]>(url).toPromise();
+
+    if(status === 200) {
+      todos = data;
+    }
+
+    return todos;
   }
+
 }
