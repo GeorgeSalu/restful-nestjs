d1f347f790f3d7afaa1b3a589806f46c72a55bd8
GeorgeSalu <george.salu10@gmail.com>
2024-04-17 18:59:12 -0300

    news-api - implementando funcionalidade de editar usuario

--- news-api/src/user/user.controller.ts
+++ news-api/src/user/user.controller.ts
@@ -1,7 +1,8 @@
-import { BadRequestException, Body, Controller, Get, NotFoundException, Param, Post, UsePipes, ValidationPipe } from "@nestjs/common";
+import { BadGatewayException, BadRequestException, Body, Controller, Get, NotFoundException, Param, Post, Put, UsePipes, ValidationPipe } from "@nestjs/common";
 import { CreateUserDto } from "./dtos/createuser.dto";
 import { Users } from "@prisma/client";
 import { UserService } from "./user.service";
+import { UpdateUserDto } from "./dtos/updateUser.dto";
 
 @Controller('users')
 export class UserController {
@@ -36,5 +37,32 @@ export class UserController {
         return user;
     }
 
+    @UsePipes(ValidationPipe)
+    @Put('/:id')
+    public async update(
+        @Body() userData: UpdateUserDto,
+        @Param('id') id: string
+    ): Promise<Users> {
+        if (!userData?.name || !userData?.email) {
+            throw new BadRequestException('name e email is required');
+        }
+
+        const userById = await this.userService.findById(id);
+
+        if (!userById) {
+            throw new NotFoundException('user not found')
+        }
+
+        const userByEmail = await this.userService.findByEmail(userData?.email);
+
+        if (userByEmail !== null) {
+            if (userByEmail?.id !== id) {
+                throw new BadRequestException('this email is not available')
+            }
+        }
+
+        const updatedUser = await this.userService.update({ id, data: userData });
+        return updatedUser;
+    }
 
 }
