497657a822532059a0d1da275240d03489ab8223
GeorgeSalu <george.salu10@gmail.com>
2024-04-17 12:10:09 -0300

    news-api - implementando a funcionalidade de criacao de usaurio parte 2

--- news-api/src/user/user.service.ts
+++ news-api/src/user/user.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from "@nestjs/common";
+import { Prisma, Users } from "@prisma/client";
+import { hash } from "bcrypt";
 import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable()
 export class UserService {
 
     constructor(
-        prismaService: PrismaService
+        private prismaService: PrismaService
     ) { }
+
+    public async create(data: Prisma.UsersCreateInput): Promise<Users> {
+        const saltOrRounds = 10;
+        const passwordHashed = await hash(data?.password, saltOrRounds);
+        const userData: Prisma.UsersCreateInput = { ...data, password: passwordHashed };
+
+        const newUser = await this.prismaService.users.create({
+            data: userData,
+            select: { news: false, created_at: false, update_at: false }
+        })
+        return newUser as Users;
+    }
 
 }
