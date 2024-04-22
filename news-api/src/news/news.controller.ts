6d453669cb524da7c347e835540847fdf84b4f02
GeorgeSalu <george.salu10@gmail.com>
2024-04-22 11:14:54 -0300

    news-api - implementando funcionalidade de criar noticia parte 2

--- news-api/src/news/news.controller.ts
+++ news-api/src/news/news.controller.ts
@@ -1,16 +1,37 @@
-import { Body, Controller, Post, UsePipes, ValidationPipe } from "@nestjs/common";
+import { Body, Controller, NotFoundException, Post, UsePipes, ValidationPipe } from "@nestjs/common";
 import { CreateNewDto } from "./dtos/CreateNews.dto";
 import { News } from "@prisma/client";
+import { UserService } from "src/user/user.service";
+import { newService } from "./news.service";
+import { CategoriesService } from "src/categories/categories.service";
 
 @Controller('news')
 export class NewsController {
 
+    constructor(
+        private userService: UserService,
+        private newsService: newService,
+        private categoriesService: CategoriesService
+    ) { }
+
     @UsePipes(ValidationPipe)
     @Post()
     public async store(@Body() createNews: CreateNewDto): Promise<News> {
+        const { author_id, category_id } = createNews;
+        const findUserById = await this.userService.findById(author_id);
 
+        if (!findUserById) {
+            throw new NotFoundException('user not found');
+        }
 
+        const findCategoryById = await this.categoriesService.findById(category_id);
 
+        if (!findCategoryById) {
+            throw new NotFoundException('category not found');
+        }
+
+        const news = await this.newsService.create(createNews);
+        return news;
     }
 
 }
--- news-api/src/news/news.module.ts
+++ news-api/src/news/news.module.ts
@@ -2,10 +2,12 @@ import { Module } from "@nestjs/common";
 import { NewsController } from "./news.controller";
 import { PrismaService } from "src/prisma/prisma.service";
 import { newService } from "./news.service";
+import { UserService } from "src/user/user.service";
+import { CategoriesService } from "src/categories/categories.service";
 
 @Module({
     controllers: [NewsController],
-    providers: [PrismaService, newService],
+    providers: [PrismaService, newService, UserService, CategoriesService],
     exports: [newService]
 })
 export class NewsModule {
