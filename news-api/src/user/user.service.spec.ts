438c3f21723b8fcebe82cc6489743ffd762251cb
GeorgeSalu <george.salu10@gmail.com>
2024-04-23 10:09:18 -0300

    news-api - implementando testes unitarios de service de users parte 1

---
+++ news-api/src/user/user.service.spec.ts
@@ -0,0 +1,13 @@
+import { PrismaService } from "../prisma/prisma.service";
+import { UserService } from "./user.service";
+
+describe(`${UserService.name}`, () => {
+
+    let service: UserService;
+    let prismaService: PrismaService
+
+    beforeEach(async () => {
+
+    })
+
+})
