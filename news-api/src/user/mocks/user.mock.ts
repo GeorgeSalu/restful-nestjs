54936a40564e4e56d0ee06a0f49a8bce1384445c
GeorgeSalu <george.salu10@gmail.com>
2024-04-23 10:19:31 -0300

    news-api - implementando testes unitarios de service de users parte 1

---
+++ news-api/src/user/mocks/user.mock.ts
@@ -0,0 +1,31 @@
+import { Users } from "@prisma/client";
+import { create } from "domain";
+
+export const userMock: Users[] = [
+    {
+        id: "af1c2549-b456-414c-b7ab-110369b0f8d4",
+        name: "marcos alterado",
+        email: "teste@gmail.com",
+        password: "$2b$10$4H8dcl5qFPwsbFY0MzpSpe7zcR3iOe355o2A8mt2ZlG9CEG8j4eOa",
+        created_at: new Date(),
+        update_at: new Date()
+    },
+    {
+        id: "551c2549-b456-414c-b7ab-110369b0f8d4",
+        name: "marcos ",
+        email: "teste2@gmail.com",
+        password: "$2b$10$Ei9OQE2k33UDKrAMWsLwd.KhC0CXiPIupS6rCxoi1VwpBpDtFALSy",
+        created_at: new Date(),
+        update_at: new Date()
+    }
+];
+
+export const prismaUSerMock = {
+    users: {
+        create: jest.fn().mockReturnValue(userMock[0]),
+        findMany: jest.fn().mockReturnValue(userMock),
+        findFirst: jest.fn().mockReturnValue(userMock[0]),
+        update: jest.fn().mockReturnValue(userMock[0]),
+        delete: jest.fn(),
+    }
+}
--- news-api/src/user/user.service.spec.ts
+++ news-api/src/user/user.service.spec.ts
@@ -1,5 +1,7 @@
+import { Test, TestingModule } from "@nestjs/testing";
 import { PrismaService } from "../prisma/prisma.service";
 import { UserService } from "./user.service";
+import { prismaUSerMock } from "./mocks/user.mock";
 
 describe(`${UserService.name}`, () => {
 
@@ -8,6 +10,18 @@ describe(`${UserService.name}`, () => {
 
     beforeEach(async () => {
 
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                UserService,
+                {
+                    provide: PrismaService, useValue: prismaUSerMock
+                }
+            ],
+
+        }).compile();
+
+        service = module.get<UserService>(UserService);
+        prismaService = module.get<PrismaService>(PrismaService)
     })
 
 })
