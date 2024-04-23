7c6d3865119ee3d012ccfabb05ecb6321ad5df58
GeorgeSalu <george.salu10@gmail.com>
2024-04-23 14:19:43 -0300

    news-api - implementando testes unitarios de service de news parte 2

--- news-api/src/news/news.service.spec.ts
+++ news-api/src/news/news.service.spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { PrismaService } from "../prisma/prisma.service";
 import { NewsService } from "./news.service"
-import { prismaNewsMock } from "./mocks/news.mock";
+import { newsMock, prismaNewsMock } from "./mocks/news.mock";
 
 describe(`${NewsService.name}`, () => {
     let service: NewsService;
@@ -21,6 +21,27 @@ describe(`${NewsService.name}`, () => {
 
     beforeEach(() => {
         jest.clearAllMocks();
+    })
+
+    it(`${NewsService.prototype.create.name}() should create new news`, async () => {
+        const response = await service.create(newsMock[0]);
+
+        expect(response).toEqual(newsMock[0]);
+        expect(prismaService.news.create).toHaveBeenCalledTimes(1);
+    })
+
+    it(`${NewsService.prototype.findAll.name} should return all news`, async () => {
+        const response = await service.findAll();
+
+        expect(response).toEqual(newsMock);
+        expect(prismaService.news.findMany).toHaveBeenCalledTimes(1);
+    })
+
+    it(`${NewsService.prototype.findById.name} should return a single new`, async () => {
+        const response = await service.findById(newsMock[0].id);
+
+        expect(response).toEqual(newsMock[0]);
+        expect(prismaService.news.findFirst).toHaveBeenCalledTimes(1);
     })
 
 })
