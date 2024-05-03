addc02bfd9d9b7abf8b9a6126121b9d8659acb8e
GeorgeSalu <george.salu10@gmail.com>
2024-05-03 17:36:41 -0300

    devtraining -  trabalhando com controladores

--- devtraining/src/courses/courses.controller.ts
+++ devtraining/src/courses/courses.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('courses')
-export class CoursesController {}
+export class CoursesController {
+
+    @Get()
+    findAll() {
+        return 'listagem de cursos'
+    }
+
+}
