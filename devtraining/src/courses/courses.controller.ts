3fc5c0f4348f47b51c515dcbf7c99d0a4fbb00be
GeorgeSalu <george.salu10@gmail.com>
2024-05-06 18:50:44 -0300

    devtraining -  trabalhando com os dados de payload

--- devtraining/src/courses/courses.controller.ts
+++ devtraining/src/courses/courses.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('courses')
 export class CoursesController {
@@ -11,6 +11,11 @@ export class CoursesController {
     @Get(':id')
     findOne(@Param('id') id: string) {
         return `curso #1 ${id}`;
+    }
+
+    @Post()
+    create(@Body() body) {
+        return body;
     }
 
 }
