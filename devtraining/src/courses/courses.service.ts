fa286b6330eb3e46ae803d668db5d66c6d7fa194
GeorgeSalu <george.salu10@gmail.com>
2024-05-07 15:22:12 -0300

    devtraining -  corrige metodo update e remove

--- devtraining/src/courses/courses.service.ts
+++ devtraining/src/courses/courses.service.ts
@@ -39,11 +39,19 @@ export class CourseService {
     update(id: string, updateCourseDto: any) {
         const indexCourse = this.courses.findIndex((course: Course) => course.id === Number(id));
 
+        if (!indexCourse) {
+            throw new HttpException(`course id ${id} not found`, HttpStatus.NOT_FOUND);
+        }
+
         this.courses[indexCourse] = updateCourseDto;
     }
 
     remove(id: string) {
         const indexCourse = this.courses.findIndex((course: Course) => course.id === Number(id));
+
+        if (!indexCourse) {
+            throw new HttpException(`course id ${id} not found`, HttpStatus.NOT_FOUND);
+        }
 
         if (indexCourse >= 0) {
             this.courses.splice(indexCourse, 1);
