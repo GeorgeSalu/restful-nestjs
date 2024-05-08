7ce89bacfd0247c80d06482ef8c8f8aa2b4dc872
GeorgeSalu <george.salu10@gmail.com>
2024-05-08 10:39:38 -0300

    devtraining -  validacao de dados validationpipe parte 1

--- devtraining/src/main.ts
+++ devtraining/src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // incluindo validação de dados, instalar dependencia class-validator e class-transformer
+  // incluindo validação de dados, instalar dependências class-validator e class-transformer
   app.useGlobalPipes(new ValidationPipe())
   await app.listen(3000);
 }
