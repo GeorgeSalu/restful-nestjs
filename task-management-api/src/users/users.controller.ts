65fa102f58a421c41a012e44e6d8bd92c7105749
GeorgeSalu <george.salu10@gmail.com>
2024-10-01 20:35:45 -0300

    projeto task-management-api - cria endpoint pra criacao de usuarios

--- task-management-api/src/users/users.controller.ts
+++ task-management-api/src/users/users.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UserDto } from './user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    constructor(
+        private readonly userServices: UsersService
+    ) { }
+
+    @Post()
+    create(@Body() user: UserDto) {
+        this.userServices.create(user);
+    }
+
+}
